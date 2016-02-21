971249325a41dccb752879f94090786e370ee74f
Raj Lad <raj95_lad@hotmail.com>
2016-02-21 00:20:23 -0500

    patient entry changes

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 var express = require('express');
 var app = express();
 
-app.get('/kitty', function (req, res) {
+app.get('/food', function (req, res) {
   // do biz logic
   // var kitty = new cat({})
   // kitty.save()
+  var food_entry = new food{}
+  food{
+  	days,meals = []
+  }
+
 
 
   // Cat.findById(id);
   // Cat.find({ name: "Kitt"}, function() {})
   res.send('Hello World!');
 });
+
+app.get('/prescription', function (req, res){
+
+};
+
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
